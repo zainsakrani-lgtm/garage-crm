8cc82f139b3373e1505663bd7be91f1b25a5d61d
Zain SAKRANI (PTV Logistics) <zain.sakrani@ptvlogistics.com>
2026-01-05 05:38:53 +0100

    Convert supabase client to ES module

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,10 +1,12 @@
-const { createClient } = require("@supabase/supabase-js");
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
-const supabase = createClient(
+dotenv.config();
+
+export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
 );
 
-module.exports = supabase;
 
 
